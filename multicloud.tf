eb6f135c256d80a1cc28c5ef3c03606b5ce5bff7
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-06-25 11:39:27 +0100

    remove aws cluster

--- multicloud.tf
+++ multicloud.tf
@@ -150,51 +150,51 @@ module "azurecloud" {
 }
 
 
-module "awscloud" {
-  source = "github.com/ibm-cloud-architecture/terraform-icp-aws.git?ref=v1.1"
-
-  providers = {
-    "aws" = "aws.awsclusters"
-  }
-
-  aws_region = "eu-west-1"
-
-  ami = "ami-0cbf7a0c36bde57c9"
-
-  bastion = {
-    nodes = "1"
-    type      = "t2.micro"
-    ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
-    disk      = "10" //GB
-  }
-
-  master = {
-    nodes = "1"
-    type      = "m4.2xlarge"
-    ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
-    disk      = "300" //GB
-    docker_vol = "100" // GB
-    ebs_optimized = true    // not all instance types support EBS optimized
-  }
-
-  proxy = {
-    nodes     = "1"
-    type      = "m4.xlarge"
-    ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
-    disk      = "150" //GB
-    docker_vol = "100" // GB
-    ebs_optimized = true    // not all instance types support EBS optimized
-  }
-
-  key_name = "hk_key"
-
-  icp_inception_image = "ibmcom/icp-inception-amd64:3.2.0"
-
-  icp_network_cidr = "172.23.0.0/17"
-
-  icp_service_network_cidr = "172.23.128.0/17"
-
-}
+# module "awscloud" {
+#   source = "github.com/ibm-cloud-architecture/terraform-icp-aws.git?ref=v1.1"
+#
+#   providers = {
+#     "aws" = "aws.awsclusters"
+#   }
+#
+#   aws_region = "eu-west-1"
+#
+#   ami = "ami-0cbf7a0c36bde57c9"
+#
+#   bastion = {
+#     nodes = "1"
+#     type      = "t2.micro"
+#     ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
+#     disk      = "10" //GB
+#   }
+#
+#   master = {
+#     nodes = "1"
+#     type      = "m4.2xlarge"
+#     ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
+#     disk      = "300" //GB
+#     docker_vol = "100" // GB
+#     ebs_optimized = true    // not all instance types support EBS optimized
+#   }
+#
+#   proxy = {
+#     nodes     = "1"
+#     type      = "m4.xlarge"
+#     ami       = "ami-0cbf7a0c36bde57c9" // Leave blank to let terraform search for Ubuntu 16.04 ami. NOT RECOMMENDED FOR PRODUCTION
+#     disk      = "150" //GB
+#     docker_vol = "100" // GB
+#     ebs_optimized = true    // not all instance types support EBS optimized
+#   }
+#
+#   key_name = "hk_key"
+#
+#   icp_inception_image = "ibmcom/icp-inception-amd64:3.2.0"
+#
+#   icp_network_cidr = "172.23.0.0/17"
+#
+#   icp_service_network_cidr = "172.23.128.0/17"
+#
+# }
 
 
 ## IBM Cloud Environment
