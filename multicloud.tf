e1412c083fdc1a5c3c4f0c3717c39197d28cebf8
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-06-26 21:48:30 +0100

    add ibmcloud on privatecloud account

--- multicloud.tf
+++ multicloud.tf
@@ -28,56 +28,56 @@ provider "ibm" {
   softlayer_api_key = "${var.sl_apikey}"
 }
 
-#
-# module "ibmcloud" {
-#   source = "git::https://github.com/ibm-cloud-architecture/terraform-icp-ibmcloud.git//templates/icp-ce-with-loadbalancers?ref=v1"
-#   providers = {
-#     ibm = "ibm.ibmcloudclusters"
-#   }
-#   sl_username = "hans.moen.case"
-#   sl_api_key = "${var.sl_apikey}"
-#   icp_inception_image = "3.1.2"
-#   deployment = "srerefarchimp"
-#   key_name = ["hk_key"]
-#   datacenter = "lon06"
-#
-#   worker = {
-#     nodes = "4"
-#     cpu_cores   = "4"
-#     memory      = "4096"
-#
-#     disk_size         = "100" // GB
-#     docker_vol_size   = "100" // GB
-#     local_disk  = false
-#
-#     network_speed= "1000"
-#
-#     hourly_billing = true
-#   }
-#
-#   boot = {
-#     # This will also act as the VPN Gateway after creation
-#     nodes = "1"
-#     cpu_cores         = "2"
-#     memory            = "4096"
-#
-#     disk_size         = "100" // GB
-#     docker_vol_size   = "100" // GB
-#     local_disk        = true
-#     os_reference_code = "UBUNTU_16_64"
-#
-#     network_speed     = "1000"
-#
-#     hourly_billing = true
-#
-#   }
-#
-#   network_cidr = "172.21.128.0/17"
-#
-#
-#   service_network_cidr  = "172.21.127.0/24"
-#
-# }
+
+module "ibmcloud" {
+  source = "git::https://github.com/ibm-cloud-architecture/terraform-icp-ibmcloud.git//templates/icp-ce-with-loadbalancers?ref=v1"
+  providers = {
+    ibm = "ibm.ibmcloudclusters"
+  }
+  sl_username = "hans.moen.case"
+  sl_api_key = "${var.sl_apikey}"
+  icp_inception_image = "3.1.2"
+  deployment = "srerefarchimp"
+  key_name = ["hk_key"]
+  datacenter = "lon06"
+
+  worker = {
+    nodes = "4"
+    cpu_cores   = "4"
+    memory      = "4096"
+
+    disk_size         = "100" // GB
+    docker_vol_size   = "100" // GB
+    local_disk  = false
+
+    network_speed= "1000"
+
+    hourly_billing = true
+  }
+
+  boot = {
+    # This will also act as the VPN Gateway after creation
+    nodes = "1"
+    cpu_cores         = "2"
+    memory            = "4096"
+
+    disk_size         = "100" // GB
+    docker_vol_size   = "100" // GB
+    local_disk        = true
+    os_reference_code = "UBUNTU_16_64"
+
+    network_speed     = "1000"
+
+    hourly_billing = true
+
+  }
+
+  network_cidr = "172.21.128.0/17"
+
+
+  service_network_cidr  = "172.21.127.0/24"
+
+}
 
 module "azurecloud" {
   source = "github.com/ibm-cloud-architecture/terraform-icp-azure.git//templates/icp-ce?ref=v1"
@@ -197,29 +197,29 @@ module "awscloud" {
 
 
 ## IBM Cloud Environment
-# output "ibmcloud_icp_console_host" {
-#   value = "${module.ibmcloud.icp_console_host}"
-# }
-#
-# output "ibmcloud_icp_proxy_host" {
-#   value = "${module.ibmcloud.icp_proxy_host}"
-# }
-#
-# output "ibmcloud_icp_console_url" {
-#   value = "${module.ibmcloud.icp_console_url}"
-# }
-#
-# output "ibmcloud_icp_registry_url" {
-#   value = "${module.ibmcloud.icp_registry_url}"
-# }
-#
-# output "ibmcloud_kubernetes_api_url" {
-#   value = "${module.ibmcloud.kubernetes_api_url}"
-# }
-#
-# output "ibmcloud_icp_admin_username" {
-#   value = "admin"
-# }
+output "ibmcloud_icp_console_host" {
+  value = "${module.ibmcloud.icp_console_host}"
+}
+
+output "ibmcloud_icp_proxy_host" {
+  value = "${module.ibmcloud.icp_proxy_host}"
+}
+
+output "ibmcloud_icp_console_url" {
+  value = "${module.ibmcloud.icp_console_url}"
+}
+
+output "ibmcloud_icp_registry_url" {
+  value = "${module.ibmcloud.icp_registry_url}"
+}
+
+output "ibmcloud_kubernetes_api_url" {
+  value = "${module.ibmcloud.kubernetes_api_url}"
+}
+
+output "ibmcloud_icp_admin_username" {
+  value = "admin"
+}
 
 ## We will not output the passwords since
 ## we do our builds in public
