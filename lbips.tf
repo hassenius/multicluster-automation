423f1beb80512344a8d6ff388bd78f8eb2bc8fdc
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-06-26 16:48:38 +0100

    remove subnet

--- lbips.tf
+++ lbips.tf
@@ -2,10 +2,10 @@ provider "ibm" {
 
 }
 
-resource "ibm_subnet" "lbips" {
-  type = "Portable"
-  private = false
-  ip_version = 4
-  capacity = 8
-  vlan_id = 2154287
-}
+# resource "ibm_subnet" "lbips" {
+#   type = "Portable"
+#   private = false
+#   ip_version = 4
+#   capacity = 8
+#   vlan_id = 2154287
+# }
