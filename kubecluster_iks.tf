38a059e82a8d9e20f1df8efca58b98df084f0963
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-07-18 15:09:46 +0100

    add iks openshift cluster

---
+++ kubecluster_iks.tf
@@ -0,0 +1,42 @@
+variable "iks_resource_group" { }
+variable "iks_public_vlan" { }
+variable "iks_private_vlan" { }
+variable "iks_slack_webhook" { }
+variable "iks_region" { default = "eu-gb" }
+
+data "ibm_resource_group" "refarch" {
+  name = "${var.iks_resource_group}"
+}
+
+
+resource "ibm_container_cluster" "iks_refarch_cluster" {
+  name            = "sre_refarch"
+  datacenter      = "lon06"
+  kube_version    = "3.11.104_openshift"
+  machine_type    = "u3c.2x4"
+  hardware        = "shared"
+  public_vlan_id  = "${var.iks_public_vlan}"
+  private_vlan_id = "${var.iks_private_vlan}"
+  #subnet_id       = ["1154643"]
+  region          = "${var.iks_region}"
+  resource_group_id = "${data.ibm_resource_group.refarch.id}"
+
+  default_pool_size      = 1
+
+  webhook = [{
+    level = "Normal"
+    type = "slack"
+    url = "${var.iks_slack_webhook}"
+  }]
+
+}
+
+data "ibm_container_cluster_config" "refarch_ds_cluster" {
+  cluster_name_id = "${ibm_container_cluster.iks_refarch_cluster.id}"
+  resource_group_id = "${data.ibm_resource_group.refarch.id}"
+  region          = "${var.iks_region}"
+}
+
+provider "kubernetes" {
+  config_path = "${data.ibm_container_cluster_config.refarch_ds_cluster.config_file_path}"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+# Default values for IKS Cluster
+iks_resource_group = "SREREFARCH"
+iks_public_vlan    = "2651191"
+iks_private_vlan   = "2651193"
+
+## We'll maintain this as a secret in the CI Env Vars
+# iks_slack_webhook  = ""
